05262b129e16eeab7bbd1011e3359d4488b5a9ac
Sashiki05 <save.cutolo@gmail.comm>
2023-03-28 19:09:17 +0200

    afficher des donnes dans un composant

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,28 @@
 
 function PokemonCard () {
+  const pokemon = pokemonList[0];
   return (
-    <figure>
-      <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="bulbasaur" />
-    </figure>
-  );
+    <div>
+      {pokemon.name === "bulbasaur" ? (
+        <figure>
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        </figure> 
+      ) : (<p> ???</p>)
+      } 
+    </div>
+    );
 };
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 
 export default PokemonCard;
 
