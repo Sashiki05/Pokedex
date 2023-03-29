a9951753ba86c1f1ffb4528933ce4663a9d69146
Sashiki05 <save.cutolo@gmail.comm>
2023-03-29 21:53:25 +0200

    add proptypes pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
+
 function App() {
-  const [count, setCount] = useState(0)
   const pokemonList = [
     {
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
-    {
-      name: "mew",
-    },
+    {name: "mew"},
   ];
   return (
     <PokemonCard pokemon={pokemonList[0]} />
   )
 }
 
-{}
+
+
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,26 @@
+import PropTypes from "prop-types";
 
 function PokemonCard ({ pokemon })  {
 
   return (
     <div>
-      {pokemon.imgSrc == undefined  ? 
-      <p> ???</p> 
-      :
-      (
+        {pokemon.imgSrc == undefined  ? 
+        <p> ???</p> 
+        :
+        (
         <figure>
         <img src={pokemon.imgSrc} alt={pokemon.name} />
         </figure> ) 
       } 
     </div>
-    );
-};
+    );}
 
+    PokemonCard.propTypes = {
+      pokemon: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string.isRequired,
+      })
+    };
 
 export default PokemonCard;
 
