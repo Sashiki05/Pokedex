44c6295eef82ba1c24c6033dbac06ec07ad5f395
Sashiki05 <save.cutolo@gmail.comm>
2023-03-29 16:42:03 +0200

    quete 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,28 +1,20 @@
 
-function PokemonCard () {
-  const pokemon = pokemonList[0];
+function PokemonCard ({ pokemon })  {
+
   return (
     <div>
-      {pokemon.name === "bulbasaur" ? (
+      {pokemon.imgSrc == undefined  ? 
+      <p> ???</p> 
+      :
+      (
         <figure>
         <img src={pokemon.imgSrc} alt={pokemon.name} />
-        </figure> 
-      ) : (<p> ???</p>)
+        </figure> ) 
       } 
     </div>
     );
 };
 
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
 
 export default PokemonCard;
 
