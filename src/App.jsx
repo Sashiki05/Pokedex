19f6899b29c13499e2ab5b8880e3fa2861657d88
Sashiki05 <save.cutolo@gmail.comm>
2023-03-30 11:47:19 +0200

    add state pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,55 @@ import { useState } from 'react'
 import './App.css'
 import PokemonCard from './components/PokemonCard'
 
-
-function App() {
-  const pokemonList = [
-    {
+const pokemonList = [
+  {
       name: "bulbasaur",
       imgSrc:
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
-    {name: "mew"},
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+    
   ];
-  return (
-    <PokemonCard pokemon={pokemonList[0]} />
-  )
-}
 
+function App() {
+  
+ const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handlePrevClick = () => {
+    setPokemonIndex( pokemonIndex- 1);
+  };
+
+  const handleNextClick = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  return ( 
+    <div>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 ? <button onClick={handlePrevClick}>Previous</button> : null}
+      {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleNextClick}>Next</button> : null}
+    </div>
+  )
+  
+
+ }
 
 
 export default App
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -18,7 +18,7 @@ function PokemonCard ({ pokemon })  {
     PokemonCard.propTypes = {
       pokemon: PropTypes.shape({
         name: PropTypes.string.isRequired,
-        imgSrc: PropTypes.string.isRequired,
+        imgSrc: PropTypes.string,
       })
     };
 
