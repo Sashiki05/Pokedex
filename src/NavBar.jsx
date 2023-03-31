80aefe460513eceaf7a6e035f94583f1b04a4e1c
Sashiki05 <save.cutolo@gmail.comm>
2023-03-31 17:00:23 +0200

    add button navibar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,22 +1,40 @@
-function NavBar({pokemonIndex, setPokemonIndex, pokemonList}) {
-  
-    const handlePrevClick = () => {setPokemonIndex( pokemonIndex- 1);};
+    function NavBar({pokemonIndex, setPokemonIndex, pokemonList}) {
+        return (
+            
+            <div className="pokemon-container">
+                {pokemonList.map((element, pokemonIndex) => {
+                const handleClickButton = () => {
+                    setPokemonIndex(pokemonIndex);
+                };
+                return (
+                    <button key={element.name} onClick={handleClickButton}>
+                        {element.name}
+                    </button>
+                    
+                    
+                );
+                })}
+            </div>
+            
+        );
+        }
+    // const handlePrevClick = () => {setPokemonIndex( pokemonIndex- 1);};
    
-    const handleNextClick = () => {setPokemonIndex(pokemonIndex + 1);};
+    // const handleNextClick = () => {setPokemonIndex(pokemonIndex + 1);};
 
-    if (pokemonIndex === 3) {
-        setTimeout(() => {  
-            alert("pika pikachu !!!")
-             }, 200);
-             }
-             []
+    // if (pokemonIndex === 3) {
+    //     setTimeout(() => {  
+    //         alert("pika pikachu !!!")
+    //          }, 200);
+    //          }
+    //          []
     
    
-    return ( 
-          <div className='buttons-container'>
-          {pokemonIndex > 0 ? <button onClick={handlePrevClick}>Back</button> : null}
-          {pokemonIndex < pokemonList.length - 1 ? <button className="button" onClick={handleNextClick}>Next</button> : null}
-          </div>
-          ) 
-         }
+    // return ( 
+    //       <div className='buttons-container'>
+    //       {pokemonIndex > 0 ? <button onClick={handlePrevClick}>Back</button> : null}
+    //       {pokemonIndex < pokemonList.length - 1 ? <button className="button" onClick={handleNextClick}>Next</button> : null}
+    //       </div>
+    //       ) 
+    //      }
     export default NavBar
